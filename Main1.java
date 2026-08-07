3089e9727b58613b95bf984f9ac1fe19da1f2b3d
lovely joshi <lovelyjoshi7002@gmail.com>
2026-08-07 15:27:41 +0530

    inheritance

---
+++ Main1.java
@@ -0,0 +1,19 @@
+class Animal {
+    void eat() {
+        System.out.println("animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("dog is barking");
+    }
+}
+
+public class Main1 {
+    public static void main(String args[]) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
